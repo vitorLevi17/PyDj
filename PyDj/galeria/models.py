38ec3658b3b0706f67b5bfdb27ccadf62d7994aa
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-06-10 15:14:25 -0300

    Tratando com dados

--- PyDj/galeria/models.py
+++ PyDj/galeria/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=50, null= False, blank= False)
+    legenda = models.CharField(max_length=150, null= False, blank= False)
+    descricao = models.TextField(null= False, blank= False)
+    foto = models.CharField(max_length=100, null= False, blank= False)
+
+    def __str__(self):
+        return f"Fotografia[nome = {self.nome}]"
+
+
+
+
 
