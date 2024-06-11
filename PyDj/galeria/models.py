06548d08fee7e44fcf8eafed0166b81591fa39f5
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-06-11 10:35:06 -0300

    Admin do Django
    Primeiros passos utilizando o admin do django

--- PyDj/galeria/models.py
+++ PyDj/galeria/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Fotografia(models.Model):
+
+    opcoes_categoria = [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALAXIA","Galaxia"),
+        ("PLANETA","Planeta"),
+    ]
+
     nome = models.CharField(max_length=50, null= False, blank= False)
     legenda = models.CharField(max_length=150, null= False, blank= False)
+    categoria = models.CharField(max_length=50,choices=opcoes_categoria,default='')
     descricao = models.TextField(null= False, blank= False)
     foto = models.CharField(max_length=100, null= False, blank= False)
 
--- PyDj/galeria/views.py
+++ PyDj/galeria/views.py
@@ -1,16 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Fotografia
 
 
 def index(request):
-    dados = {
-        1: {"Nome": "Nebulosa de Carina",
-            "legenda": "webbtelecospe.org / NASA / James Web"},
-        2: {"Nome": "Karina de Nebulosa",
-            "legenda": "ellonMusk.org / NASA / Rodriguez  Web"}
-    }
-    return render(request, 'galeria/index.html',{"cards":dados})
+    fotos = Fotografia.objects.all()
+    return render(request, 'galeria/index.html',{"cards":fotos})
 
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request,id):
+    fotografia = get_object_or_404(Fotografia, pk=id)
+    return render(request, 'galeria/imagem.html',{"fotografia":fotografia})
