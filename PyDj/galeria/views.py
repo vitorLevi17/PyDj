920d0dc3f0c6525f79cd9283c9f008bfb79a95f2
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-06-18 18:06:47 -0300

    Mecanismo de busca
    Forma de buscar por palavras chaves

--- PyDj/galeria/views.py
+++ PyDj/galeria/views.py
@@ -11,3 +11,13 @@ def index(request):
 def imagem(request,id):
     fotografia = get_object_or_404(Fotografia, pk=id)
     return render(request, 'galeria/imagem.html',{"fotografia":fotografia})
+
+def buscar(request):
+    fotos = Fotografia.objects.order_by("-datas").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_busca = request.GET['buscar']
+        if nome_busca:
+            fotos = fotos.filter(nome__icontains=nome_busca)
+
+    return render(request,'galeria/buscar.html',{"cards":fotos})
